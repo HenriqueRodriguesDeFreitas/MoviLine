01aa661be20aeacb9bf41af60b0bacd572dee8f4
henriquerodrigues <devhenriquerodrigues971@gmail.com>
2025-11-18 19:29:15 -0300

    feat: add schema para exemplo de resposta de dados

--- src/main/java/com/vortex/EntregaRapida/dto/response/EstadoResponseDto.java
+++ src/main/java/com/vortex/EntregaRapida/dto/response/EstadoResponseDto.java
@@ -1,7 +1,11 @@
 package com.vortex.EntregaRapida.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.util.UUID;
 
-public record EstadoResponseDto(UUID id,
-                                String nome) {
+public record EstadoResponseDto(
+        @Schema(description = "Id do estado",
+                example = "3fa85f64-5717-4562-b3fc-2c963f66afa6") UUID id,
+        @Schema(description = "Nome do estado", example = "Pará") String nome) {
 }
