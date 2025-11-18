49fd667ce3d67773405bae8dc26ab4360780e4d5
henriquerodrigues <devhenriquerodrigues971@gmail.com>
2025-11-18 19:26:40 -0300

    feat: script de criação de tabelas de endereço

---
+++ src/main/resources/db/migration/V1__criar_tabelas_endereços.sql
@@ -0,0 +1,29 @@
+create table IF NOT EXISTS estado(
+id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+nome VARCHAR(19) NOT NULL UNIQUE
+);
+
+create table IF NOT EXISTS cidade(
+id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+nome VARCHAR(32) NOT NULL,
+estado_id UUID NOT NULL,
+CONSTRAINT fk_cidade_estado_id_fkey
+ FOREIGN KEY(estado_id) REFERENCES estado(id)
+);
+
+CREATE TABLE IF NOT EXISTS bairro(
+id UUID NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
+nome VARCHAR(150) NOT NULL,
+cidade_id UUID NOT NULL,
+ CONSTRAINT fk_bairro_cidade_id_fkey
+  FOREIGN KEY(cidade_id) REFERENCES cidade(id)
+);
+
+CREATE TABLE IF NOT EXISTS endereco(
+id UUID NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
+bairro_id UUID NOT NULL,
+logradouro VARCHAR(250) NOT NULL,
+cep VARCHAR(9) NOT NULL,
+ CONSTRAINT fk_endereco_bairro_id_fkey
+  FOREIGN KEY(bairro_id) REFERENCES bairro(id)
+);
