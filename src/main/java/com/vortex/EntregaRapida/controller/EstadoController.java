d8259cc886e491d9141cec4f676ce7cc424f1eb2
henriquerodrigues <devhenriquerodrigues971@gmail.com>
2025-11-23 13:21:09 -0300

    fix: add rota para busca de todos os estados + documentação personalizada do swagger

--- src/main/java/com/vortex/EntregaRapida/controller/EstadoController.java
+++ src/main/java/com/vortex/EntregaRapida/controller/EstadoController.java
@@ -12,10 +12,9 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("estado")
@@ -36,21 +35,44 @@ public class EstadoController {
                     content = @Content(mediaType = TYPE_JSON,
                             schema = @Schema(implementation = EstadoResponseDto.class))),
             @ApiResponse(responseCode = "409",
-            description = "Conflito: Estado já cadastrado com o nome digitado",
-            content = @Content(mediaType = TYPE_JSON,
-                    schema = @Schema(implementation = ErroResponseDto.class),
-            examples = {@ExampleObject(
-                    name = "Já existe um estado cadastrado com o nome passado", value = """
-                    {
-                    "data_hora" : "2025-09-01T14:45:04",
-                    "httpsValue" : "409",
-                    "erro" : "Erro: conflito de entidade",
-                    "descricao" : "Estado já cadastrado com o nome: Pará"
-                    }
-                    """)}))
+                    description = "Conflito: Estado já cadastrado com o nome digitado",
+                    content = @Content(mediaType = TYPE_JSON,
+                            schema = @Schema(implementation = ErroResponseDto.class),
+                            examples = {@ExampleObject(
+                                    name = "Já existe um estado cadastrado com o nome passado", value = """
+                                    {
+                                    "data_hora" : "2025-09-01T14:45:04",
+                                    "httpsValue" : "409",
+                                    "erro" : "Erro: conflito de entidade",
+                                    "descricao" : "Estado já cadastrado com o nome: Pará"
+                                    }
+                                    """)}))
     })
     public ResponseEntity<EstadoResponseDto> cadastrarEstado(@RequestBody @Valid
                                                              EstadoRequestDto dto) {
         return ResponseEntity.ok(estadoService.cadastrarEstado(dto));
+    }
+
+    @GetMapping
+    @Operation(summary = "Buscar todos os estados", description = "Busca o id e nome do estado na base de dados")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Dados dos estados retornada com sucesso",
+                    content = @Content(mediaType = TYPE_JSON,
+                            schema = @Schema(implementation = EstadoResponseDto.class),
+                            examples = {@ExampleObject(
+                                    name = "Estados retornados com sucesso.", value = """
+                                    [{
+                                    "id" : "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                                    "nome" : "Pará"
+                                    },
+                                    {
+                                    "id" : "4fa85f64-5717-4569-b3fc-2c963f66afa2",
+                                    "nome" : "Paraná"
+                                    }]
+                                    """)}))
+    })
+    public ResponseEntity<List<EstadoResponseDto>> buscarTodosEstados() {
+        return ResponseEntity.ok(estadoService.buscarTodosEstados());
     }
 }
