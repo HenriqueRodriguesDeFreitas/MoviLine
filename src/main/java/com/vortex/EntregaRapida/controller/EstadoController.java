e79991a7d8546d33025afc5543b3297baa24a6cf
henriquerodrigues <devhenriquerodrigues971@gmail.com>
2025-11-23 20:52:33 -0300

    fix: criando rota para busca de estado por id, e atualizar estado

--- src/main/java/com/vortex/EntregaRapida/controller/EstadoController.java
+++ src/main/java/com/vortex/EntregaRapida/controller/EstadoController.java
@@ -1,5 +1,6 @@
 package com.vortex.EntregaRapida.controller;
 
+import com.vortex.EntregaRapida.docs.ErroExamples;
 import com.vortex.EntregaRapida.dto.request.EstadoRequestDto;
 import com.vortex.EntregaRapida.dto.response.ErroResponseDto;
 import com.vortex.EntregaRapida.dto.response.EstadoResponseDto;
@@ -11,10 +12,13 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
+import jakarta.websocket.server.PathParam;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("estado")
@@ -31,26 +35,22 @@ public class EstadoController {
     @Operation(summary = "Cadastrar novo estado", description = "Cria o cadastro de um novo estado")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200",
-                    description = "Estado cadastrado com sucesso",
+                    description = "Estado cadastrado com sucesso!",
                     content = @Content(mediaType = TYPE_JSON,
                             schema = @Schema(implementation = EstadoResponseDto.class))),
             @ApiResponse(responseCode = "409",
-                    description = "Conflito: Estado já cadastrado com o nome digitado",
+                    description = "Conflito: Já existe um estado com este nome",
                     content = @Content(mediaType = TYPE_JSON,
                             schema = @Schema(implementation = ErroResponseDto.class),
                             examples = {@ExampleObject(
-                                    name = "Já existe um estado cadastrado com o nome passado", value = """
-                                    {
-                                    "data_hora" : "2025-09-01T14:45:04",
-                                    "httpsValue" : "409",
-                                    "erro" : "Erro: conflito de entidade",
-                                    "descricao" : "Estado já cadastrado com o nome: Pará"
-                                    }
-                                    """)}))
+                                    name = "Erro 409",
+                                    value = ErroExamples.ERRO_409
+                            )}))
     })
     public ResponseEntity<EstadoResponseDto> cadastrarEstado(@RequestBody @Valid
                                                              EstadoRequestDto dto) {
-        return ResponseEntity.ok(estadoService.cadastrarEstado(dto));
+        var estado = estadoService.cadastrarEstado(dto);
+        return ResponseEntity.created(URI.create("/estado/" + estado.id())).body(estado);
     }
 
     @GetMapping
@@ -61,7 +61,7 @@ public class EstadoController {
                     content = @Content(mediaType = TYPE_JSON,
                             schema = @Schema(implementation = EstadoResponseDto.class),
                             examples = {@ExampleObject(
-                                    name = "Estados retornados com sucesso.", value = """
+                                    name = "Estados retornados com sucesso!", value = """
                                     [{
                                     "id" : "3fa85f64-5717-4562-b3fc-2c963f66afa6",
                                     "nome" : "Pará"
@@ -74,5 +74,50 @@ public class EstadoController {
     })
     public ResponseEntity<List<EstadoResponseDto>> buscarTodosEstados() {
         return ResponseEntity.ok(estadoService.buscarTodosEstados());
+    }
+
+    @GetMapping("/{id}")
+    @Operation(summary = "Busca um estado de acordo com seu id.", description = "Retorna um estado específico de acordo com seu id.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Busca retornada com sucesso, estado encontrado!",
+                    content = @Content(mediaType = TYPE_JSON,
+                            schema = @Schema(implementation = EstadoResponseDto.class))),
+            @ApiResponse(responseCode = "404", description = "Não existe estado com o id passado.",
+                    content = @Content(mediaType = TYPE_JSON, schema = @Schema(implementation = ErroResponseDto.class),
+                            examples = @ExampleObject(
+                                    name = "404",
+                                    value = ErroExamples.ERRO_404
+                            )))
+    })
+    public ResponseEntity<EstadoResponseDto> buscarEstadoPorId(
+            @PathVariable("id") UUID id) {
+        return ResponseEntity.ok(estadoService.buscarEstadoPorId(id));
+    }
+
+    @PutMapping("/{id}")
+    @Operation(summary = "Atualiza o nome de uma estado.", description = "Busca um estado pelo id, caso encontrado, atualiza o nome.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Retorna o estado atualizado com sucesso.",
+                    content = @Content(mediaType = TYPE_JSON,
+                            schema = @Schema(implementation = EstadoResponseDto.class))),
+            @ApiResponse(responseCode = "404", description = "Não existe estado com o id passado.",
+                    content = @Content(mediaType = TYPE_JSON, schema = @Schema(implementation = ErroResponseDto.class),
+                            examples = @ExampleObject(
+                                    name = "404",
+                                    value = ErroExamples.ERRO_404
+                            ))),
+            @ApiResponse(responseCode = "409",
+                    description = "Conflito: Já existe um estado com este nome",
+                    content = @Content(mediaType = TYPE_JSON,
+                            schema = @Schema(implementation = ErroResponseDto.class),
+                            examples = {@ExampleObject(
+                                    name = "Erro 409",
+                                    value = ErroExamples.ERRO_409
+                            )}))
+    })
+    public ResponseEntity<EstadoResponseDto> atualizarEstado(@PathParam("id") UUID id,
+                                                             @RequestBody String nome) {
+        return ResponseEntity.ok(estadoService.atualizarEstado(id, nome));
     }
 }
