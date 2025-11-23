c84ddb6f4d9529569fd6796bca635d76654985fe
henriquerodrigues <devhenriquerodrigues971@gmail.com>
2025-11-23 13:21:37 -0300

    fix: mudando metodos para utilização de JPQL

--- src/main/java/com/vortex/EntregaRapida/repository/EstadoRepository.java
+++ src/main/java/com/vortex/EntregaRapida/repository/EstadoRepository.java
@@ -2,10 +2,19 @@ package com.vortex.EntregaRapida.repository;
 
 import com.vortex.EntregaRapida.model.Estado;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface EstadoRepository extends JpaRepository<Estado, UUID> {
-  Optional<Estado> findByNomeIgnoreCase(String nome);
+    @Query("SELECT new Estado(e.id, e.nome) FROM Estado e WHERE LOWER(e.nome) = LOWER(:nome)")
+    Optional<Estado> findByNomeIgnoreCase(String nome);
+
+    @Query("SELECT new Estado(e.id, e.nome) FROM Estado e WHERE e.id = :id")
+    Optional<Estado> buscarEstadoSimplesPorId(UUID id);
+
+    @Query("SELECT new Estado(e.id, e.nome) FROM Estado e")
+    List<Estado> buscarTodosEstadosSimples();
 }
