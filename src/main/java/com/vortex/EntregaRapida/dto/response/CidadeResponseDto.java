bf7215778fd0b6a0910034cabe8f2ec4d39ac054
henriquerodrigues <devhenriquerodrigues971@gmail.com>
2025-11-28 23:24:22 -0300

    feat: criando record para resposta de dados

---
+++ src/main/java/com/vortex/EntregaRapida/dto/response/CidadeResponseDto.java
@@ -0,0 +1,10 @@
+package com.vortex.EntregaRapida.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import java.util.UUID;
+
+public record CidadeResponseDto(
+        @Schema(name = "Id da cidade", examples = "3fa85f64-5717-4562-b3fc-2c963f66afa6") UUID id,
+        @Schema(name = "Nome da cidade", example = "Breves") String nome) {
+}
