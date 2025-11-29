f0e906ef7db104fc9065d3ad62b751fb9003b9b4
henriquerodrigues <devhenriquerodrigues971@gmail.com>
2025-11-28 23:23:45 -0300

    feat: criando CidadeRepository

---
+++ src/main/java/com/vortex/EntregaRapida/repository/CidadeRepository.java
@@ -0,0 +1,15 @@
+package com.vortex.EntregaRapida.repository;
+
+import com.vortex.EntregaRapida.model.Cidade;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface CidadeRepository extends JpaRepository<Cidade, UUID> {
+    @Query("""
+            SELECT new Cidade(c.id, c.nome) FROM Cidade c WHERE c.estado.id = :estadoId
+           """)
+    List<Cidade> buscarCidadesPorEstado(UUID estadoId);
+}
