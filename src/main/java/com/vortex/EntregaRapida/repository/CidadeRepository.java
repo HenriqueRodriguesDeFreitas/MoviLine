242f247915610dac9f96455764d728828333e870
henriquerodrigues <devhenriquerodrigues971@gmail.com>
2025-11-30 18:24:37 -0300

    fix: add metodo para buscar cidade por id

--- src/main/java/com/vortex/EntregaRapida/repository/CidadeRepository.java
+++ src/main/java/com/vortex/EntregaRapida/repository/CidadeRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface CidadeRepository extends JpaRepository<Cidade, UUID> {
@@ -12,4 +13,9 @@ public interface CidadeRepository extends JpaRepository<Cidade, UUID> {
             SELECT new Cidade(c.id, c.nome) FROM Cidade c WHERE c.estado.id = :estadoId
            """)
     List<Cidade> buscarCidadesPorEstado(UUID estadoId);
+
+    @Query("""
+            SELECT new Cidade(c.id, c.nome) FROM Cidade c WHERE c.id = :cidadeId
+            """)
+    Optional<Cidade> buscarCidadeSimplesPorId(UUID cidadeId);
 }
