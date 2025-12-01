5e3d49cc712e025205fd669870d6a3de76d6118d
henriquerodrigues <devhenriquerodrigues971@gmail.com>
2025-12-01 19:00:01 -0300

    fix: add outro construtor

--- src/main/java/com/vortex/EntregaRapida/model/Bairro.java
+++ src/main/java/com/vortex/EntregaRapida/model/Bairro.java
@@ -1,5 +1,6 @@
 package com.vortex.EntregaRapida.model;
 
+import com.vortex.EntregaRapida.exception.custom.ConflitoDeEntidadeException;
 import jakarta.persistence.*;
 
 import java.util.UUID;
@@ -23,9 +24,16 @@ public class Bairro {
     }
 
     public Bairro(UUID id, String nome, Cidade cidade) {
-        if(nome == null) throw new IllegalArgumentException("Nome do bairro não pode ser nulo");
-        if(cidade == null) throw new IllegalArgumentException("Bairro precisa ser vinculado a uma cidade");
+        if (nome == null) throw new IllegalArgumentException("Nome do bairro não pode ser nulo");
+        if (cidade == null) throw new IllegalArgumentException("Bairro precisa ser vinculado a uma cidade");
         this.id = id;
+        this.nome = nome;
+        this.cidade = cidade;
+    }
+
+    public Bairro(String nome, Cidade cidade) {
+        if (nome == null) throw new IllegalArgumentException("Nome do bairro não pode ser nulo");
+        if (cidade == null) throw new ConflitoDeEntidadeException("Bairro precisa ser vinculado a uma cidade");
         this.nome = nome;
         this.cidade = cidade;
     }
@@ -43,7 +51,7 @@ public class Bairro {
     }
 
     public void setNome(String nome) {
-        if(nome == null) throw new IllegalArgumentException("Nome do bairro não pode ser nulo");
+        if (nome == null) throw new IllegalArgumentException("Nome do bairro não pode ser nulo");
         this.nome = nome;
     }
 
@@ -52,7 +60,7 @@ public class Bairro {
     }
 
     public void setCidade(Cidade cidade) {
-        if(cidade == null) throw new IllegalArgumentException("Bairro precisa ser vinculado a uma cidade");
+        if (cidade == null) throw new IllegalArgumentException("Bairro precisa ser vinculado a uma cidade");
         this.cidade = cidade;
     }
 }
