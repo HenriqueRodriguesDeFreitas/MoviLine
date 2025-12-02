ef7ad42f3f641cd797ef6adacfe85baf7593fa91
henriquerodrigues <devhenriquerodrigues971@gmail.com>
2025-12-02 19:59:09 -0300

    fix: add metodo para buscar bairro por id;

--- src/main/java/com/vortex/EntregaRapida/repository/BairroRepository.java
+++ src/main/java/com/vortex/EntregaRapida/repository/BairroRepository.java
@@ -3,9 +3,14 @@ package com.vortex.EntregaRapida.repository;
 
 import com.vortex.EntregaRapida.model.Bairro;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface BairroRepository extends JpaRepository<Bairro, UUID> {
-
+    @Query("""
+            SELECT new Bairro(b.id, b.nome) FROM Bairro b WHERE b.id = :bairroId
+            """)
+    Optional<Bairro> buscarBairroPorId(UUID bairroId);
 }
