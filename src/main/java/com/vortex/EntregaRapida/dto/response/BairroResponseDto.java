8edba7ce5c8df1718fdb71810509b2095f0df779
henriquerodrigues <devhenriquerodrigues971@gmail.com>
2025-12-03 19:42:01 -0300

    feat: add variável do tipo UUID.

--- src/main/java/com/vortex/EntregaRapida/dto/response/BairroResponseDto.java
+++ src/main/java/com/vortex/EntregaRapida/dto/response/BairroResponseDto.java
@@ -4,6 +4,8 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.util.UUID;
 
-public record BairroResponseDto(@Schema(name = "Id", description = "3fa85f64-5717-4562-b3fc-2c963f66afa6") UUID id,
-                                @Schema(name = "Nome", description = "Aeroporto") String nome) {
+public record BairroResponseDto(@Schema(name = "Id",
+        description = "3fa85f64-5717-4562-b3fc-2c963f66afa6") UUID id,
+                                @Schema(name = "Nome",
+                                        description = "Aeroporto") String nome) {
 }
