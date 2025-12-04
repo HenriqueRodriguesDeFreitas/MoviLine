a9ff88503c3486a6c0a039aced6eb423d45de861
henriquerodrigues <devhenriquerodrigues971@gmail.com>
2025-12-04 19:44:35 -0300

    feat: criando bairro repository

--- src/main/java/com/vortex/EntregaRapida/repository/BairroRepository.java
+++ src/main/java/com/vortex/EntregaRapida/repository/BairroRepository.java
@@ -5,6 +5,7 @@ import com.vortex.EntregaRapida.model.Bairro;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -13,4 +14,20 @@ public interface BairroRepository extends JpaRepository<Bairro, UUID> {
             SELECT new Bairro(b.id, b.nome) FROM Bairro b WHERE b.id = :bairroId
             """)
     Optional<Bairro> buscarBairroPorId(UUID bairroId);
+
+    @Query("""
+            SELECT new Bairro(b.id, b.nome) 
+            FROM Bairro b 
+            WHERE LOWER(b.nome)
+            LIKE LOWER (CONCAT('%', :nome, '%'))
+            """)
+    List<Bairro> buscarBairroPorNome(String nome);
+
+    @Query("""
+           SELECT new Bairro(b.id, b.nome) 
+           FROM Bairro b 
+           WHERE b.cidade.id = :cidadeId 
+           AND LOWER (b.nome) LIKE LOWER(CONCAT('%', :nome, '%'))
+           """)
+    List<Bairro> buscarBairroPorCidadeENome(UUID cidadeId, String nome);
 }
