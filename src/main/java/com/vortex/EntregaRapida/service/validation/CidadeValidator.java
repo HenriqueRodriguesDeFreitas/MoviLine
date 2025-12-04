909ee6acd75fed5b0f17d1a516fd459f4e3d9566
henriquerodrigues <devhenriquerodrigues971@gmail.com>
2025-12-04 18:08:49 -0300

    fix: add nova sobrecarga de metodos, comparando por Id.

--- src/main/java/com/vortex/EntregaRapida/service/validation/CidadeValidator.java
+++ src/main/java/com/vortex/EntregaRapida/service/validation/CidadeValidator.java
@@ -10,17 +10,26 @@ public final class CidadeValidator {
     public CidadeValidator() {
     }
 
-    //Usar para o cadastro de uma nova cidade
+    /*Usar para o cadastro de uma nova cidade
+     * Verifica se estado já possui uma cidade com nome passado.*/
     public static boolean estadoPossuiCidade(String nomeNovaCidade, List<Cidade> cidades) {
         return cidades.stream()
                 .anyMatch(c -> c.getNome().equalsIgnoreCase(nomeNovaCidade));
     }
 
-    //Usar para atualizar dados de uma cidade
+    /*Usar para atualizar dados de uma cidade
+     * Verifica se os ids são diferentes, mas nomes são iguais.*/
     public static boolean estadoPossuiCidade(String novoNome, List<Cidade> cidades,
                                              Cidade cidadeAtual) {
         return cidades.stream()
                 .anyMatch(c -> !c.getId().equals(cidadeAtual.getId())
                         && c.getNome().equalsIgnoreCase(novoNome));
+    }
+
+    /*Usar para metodo de deletar
+    * Verifica se o estado já possui uma cidade com o id passado*/
+    public static boolean estadoPossuiCidade(UUID cidadeId, List<Cidade> cidades) {
+        return cidades.stream()
+                .anyMatch(c -> c.getId().equals(cidadeId));
     }
 }
