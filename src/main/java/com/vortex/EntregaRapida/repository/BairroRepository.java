8dd71eba3b2bb97e751d713b6c8fc3359fa26c47
henriquerodrigues <devhenriquerodrigues971@gmail.com>
2025-12-05 00:30:02 -0300

    fix: melhorando repositories

--- src/main/java/com/vortex/EntregaRapida/repository/BairroRepository.java
+++ src/main/java/com/vortex/EntregaRapida/repository/BairroRepository.java
@@ -3,31 +3,12 @@ package com.vortex.EntregaRapida.repository;
 
 import com.vortex.EntregaRapida.model.Bairro;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 public interface BairroRepository extends JpaRepository<Bairro, UUID> {
-    @Query("""
-            SELECT new Bairro(b.id, b.nome) FROM Bairro b WHERE b.id = :bairroId
-            """)
-    Optional<Bairro> buscarBairroPorId(UUID bairroId);
+    List<Bairro> findByNomeContainingIgnoreCase(String nome);
 
-    @Query("""
-            SELECT new Bairro(b.id, b.nome) 
-            FROM Bairro b 
-            WHERE LOWER(b.nome)
-            LIKE LOWER (CONCAT('%', :nome, '%'))
-            """)
-    List<Bairro> buscarBairroPorNome(String nome);
-
-    @Query("""
-           SELECT new Bairro(b.id, b.nome) 
-           FROM Bairro b 
-           WHERE b.cidade.id = :cidadeId 
-           AND LOWER (b.nome) LIKE LOWER(CONCAT('%', :nome, '%'))
-           """)
-    List<Bairro> buscarBairroPorCidadeENome(UUID cidadeId, String nome);
+    List<Bairro> findByNomeAndCidadeId(String nome, UUID cidadeId);
 }
