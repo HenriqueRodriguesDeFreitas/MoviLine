f3b60c49cf72d13d43992b9b9e6381088d710a16
henriquerodrigues <devhenriquerodrigues971@gmail.com>
2025-12-05 00:30:34 -0300

    fix: melhorando repositories

--- src/main/java/com/vortex/EntregaRapida/repository/EstadoRepository.java
+++ src/main/java/com/vortex/EntregaRapida/repository/EstadoRepository.java
@@ -2,25 +2,13 @@ package com.vortex.EntregaRapida.repository;
 
 import com.vortex.EntregaRapida.model.Estado;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface EstadoRepository extends JpaRepository<Estado, UUID> {
-    @Query("SELECT new Estado(e.id, e.nome) FROM Estado e WHERE LOWER(e.nome) = LOWER(:nome)")
     Optional<Estado> findByNomeIgnoreCase(String nome);
 
-    @Query("SELECT new Estado(e.id, e.nome) FROM Estado e WHERE e.id = :id")
-    Optional<Estado> buscarEstadoSimplesPorId(UUID id);
-
-    @Query("SELECT new Estado(e.id, e.nome) FROM Estado e")
-    List<Estado> buscarTodosEstadosSimples();
-
-    @Query("""
-            SELECT new Estado(e.id, e.nome) 
-            FROM Estado e WHERE LOWER(e.nome) LIKE LOWER(CONCAT('%', :nome, '%'))
-            """)
-    List<Estado> buscarEstadoPorNomeContaining(String nome);
+    List<Estado> findByNomeIgnoreCaseContaining(String nome);
 }
