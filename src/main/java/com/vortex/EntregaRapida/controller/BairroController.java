7fd3886558265c24c5f2d68b51eb6908fdee451d
paulo <devhenriquerodrigues971@gmail.com>
2025-12-22 20:37:13 -0300

    feat: criando controller para bairros.

---
+++ src/main/java/com/vortex/EntregaRapida/controller/BairroController.java
@@ -0,0 +1,88 @@
+package com.vortex.EntregaRapida.controller;
+
+import com.vortex.EntregaRapida.docs.ErroExamples;
+import com.vortex.EntregaRapida.dto.request.BairroRequestDto;
+import com.vortex.EntregaRapida.dto.response.BairroResponseDto;
+import com.vortex.EntregaRapida.dto.response.ErroResponseDto;
+import com.vortex.EntregaRapida.service.BairroService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@Tag(name = "Bairro", description = "Operações relacionadas ao cadastro e manutenções de bairros")
+@RestController
+@RequestMapping("bairro")
+public class BairroController {
+
+    private final BairroService bairroService;
+    private static final String TYPE_JSON = "application/json";
+    private static final String DESC_CODE_404 = "Erro de recurso não encontrado.";
+    private static final String DESC_CODE_409 = "Erro de conflito entre entidades.";
+
+    public BairroController(BairroService bairroService) {
+        this.bairroService = bairroService;
+    }
+
+    @PostMapping
+    @Operation(summary = "Cadastrar bairro",
+            description = "Método usado para cadastrar um novo bairro em uma cidade")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201",
+                    description = "Bairro cadastrado com sucesso.",
+                    content = @Content(mediaType = TYPE_JSON, schema =
+                    @Schema(implementation = BairroResponseDto.class))),
+
+            @ApiResponse(responseCode = "404",
+                    description = DESC_CODE_404,
+                    content = @Content(mediaType = TYPE_JSON, schema =
+                    @Schema(implementation = ErroResponseDto.class),
+                            examples = @ExampleObject(name = "Estado não possui cidade com o id informado.",
+                                    value = ErroExamples.ERRO_404))),
+            @ApiResponse(responseCode = "409",
+                    description = DESC_CODE_409,
+                    content = @Content(mediaType = TYPE_JSON, schema =
+                    @Schema(implementation = ErroResponseDto.class),
+                            examples = @ExampleObject(name = "Cidade já possui bairro com mesmo nome",
+                                    value = ErroExamples.ERRO_409)))
+    })
+    public ResponseEntity<BairroResponseDto> cadastrarBairro(@RequestBody @Valid BairroRequestDto dto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(bairroService.cadastrarBairro(dto));
+    }
+
+    @PutMapping("/{idBairro}")
+    @Operation(summary = "Atualizar bairro", description = "Método usado para atualizar dados de um bairro.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Dados atualizados com sucesso.",
+                    content = @Content(mediaType = TYPE_JSON, schema =
+                    @Schema(implementation = BairroResponseDto.class))),
+
+            @ApiResponse(responseCode = "404", description = DESC_CODE_404,
+                    content = @Content(mediaType = TYPE_JSON, schema =
+                    @Schema(implementation = ErroResponseDto.class),
+                            examples = {@ExampleObject(name = "Nenhum bairro encontrado com o id informado.",
+                                    value = ErroExamples.ERRO_404),
+                                    @ExampleObject(name = "Estado não possui cidade com o id informado.",
+                                            value = ErroExamples.ERRO_404)})),
+
+            @ApiResponse(responseCode = "409", description = DESC_CODE_409,
+                    content = @Content(mediaType = TYPE_JSON, schema =
+                    @Schema(implementation = ErroResponseDto.class),
+                            examples = {@ExampleObject(name = "Cidade já possui bairro com mesmo nome.",
+                                    value = ErroExamples.ERRO_409)}
+                    ))
+    })
+    public ResponseEntity<BairroResponseDto> atualizarBairro(@PathVariable("idBairro") UUID id,
+                                                             @RequestBody @Valid BairroRequestDto dto) {
+        return ResponseEntity.ok(bairroService.atualizarBairro(id, dto));
+    }
+}
