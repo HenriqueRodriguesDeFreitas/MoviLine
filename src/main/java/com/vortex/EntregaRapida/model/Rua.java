d73691f777b86d05f0030cad99bc10d507ed22b6
Paulo <devhenriquerodrigues971@gmail.com>
2026-01-04 14:08:09 -0300

    fix: mudando nome da classe

--- src/main/java/com/vortex/EntregaRapida/model/Rua.java
+++ src/main/java/com/vortex/EntregaRapida/model/Rua.java
@@ -5,28 +5,65 @@ import jakarta.persistence.*;
 import java.util.UUID;
 
 @Entity
-@Table(name = "endereco")
-public class Endereco {
+@Table(name = "rua")
+public class Rua {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     @Column(name = "rua", length = 250, nullable = false)
-    private String rua;
+    private String nome;
 
-    @Column(name = "cep", length = 9, nullable = false)
+    @Column(name = "cep", length = 8, nullable = false)
     private String cep;
 
-    @OneToOne
-    @JoinColumn(name = "estado_id", nullable = false)
-    private Estado estado;
-
-    @OneToOne
-    @JoinColumn(name = "cidade_id", nullable = false)
-    private Cidade cidade;
-
-    @OneToOne
+    @ManyToOne
     @JoinColumn(name = "bairro_id", nullable = false)
     private Bairro bairro;
+
+    public Rua() {
+    }
+
+    public Rua(UUID id, String nome, String cep, Bairro bairro) {
+        this.id = id;
+        this.nome = nome;
+        this.cep = cep;
+        this.bairro = bairro;
+    }
+
+    public Rua(String nome, String cep, Bairro bairro) {
+        this.nome = nome;
+        this.cep = cep;
+        this.bairro = bairro;
+    }
+
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public Bairro getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(Bairro bairro) {
+        this.bairro = bairro;
+    }
 }
