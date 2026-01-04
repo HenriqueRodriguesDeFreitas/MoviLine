69e65469cd68bfd03b61cc95807b3513554e6eb1
Paulo <devhenriquerodrigues971@gmail.com>
2026-01-03 23:07:20 -0300

    feat: criando sql de criação de tabela rua.

---
+++ src/main/resources/db/migration/V5__criar_tabela_rua.sql
@@ -0,0 +1,8 @@
+CREATE TABLE rua(
+  id UUID NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
+  nome VARCHAR(250) NOT NULL,
+  cep VARCHAR(8) NOT NULL,
+    bairro_id UUID NOT NULL,
+    CONSTRAINT fk_rua_bairro_id_fkey
+                FOREIGN KEY (bairro_id) REFERENCES bairro(id)
+);
