313aaddcc960609c51c03e860c3a8ba8dc095e84
Paulo <devhenriquerodrigues971@gmail.com>
2026-01-09 19:53:19 -0300

    fix: add validações.

--- src/main/java/com/vortex/EntregaRapida/service/validation/BairroCidadeValidator.java
+++ src/main/java/com/vortex/EntregaRapida/service/validation/BairroCidadeValidator.java
@@ -1,5 +1,8 @@
 package com.vortex.EntregaRapida.service.validation;
 
+import com.vortex.EntregaRapida.exception.custom.ConflitoEntidadeInexistente;
+import com.vortex.EntregaRapida.model.Bairro;
+import com.vortex.EntregaRapida.model.Cidade;
 import com.vortex.EntregaRapida.repository.BairroRepository;
 import com.vortex.EntregaRapida.repository.CidadeRepository;
 import org.springframework.stereotype.Component;
@@ -17,7 +20,31 @@ public class BairroCidadeValidator {
         this.cidadeRepository = cidadeRepository;
     }
 
-    public boolean cidadePossuiBairroComNomePassado(String nome, UUID cidadeId){
+    public boolean cidadePossuiBairroComNomePassado(String nome, UUID cidadeId) {
         return bairroRepository.existsByNomeIgnoreCaseAndCidadeId(nome, cidadeId);
     }
+
+    public boolean validaCidadePossuiBairro(UUID cidadeId, UUID bairroId) {
+        var cidade = getCidadePorId(cidadeId);
+        var bairro = getBairroPorId(bairroId);
+
+        if (!bairroRepository.existsByIdAndCidadeId(cidade.getId(), bairro.getId())) {
+            return false;
+        } else {
+            return true;
+        }
+
+    }
+
+    private Cidade getCidadePorId(UUID cidadeId) {
+        return cidadeRepository.findById(cidadeId)
+                .orElseThrow(() -> new ConflitoEntidadeInexistente("Cidade não encontrada"));
+    }
+
+    private Bairro getBairroPorId(UUID bairroId) {
+        return bairroRepository.findById(bairroId)
+                .orElseThrow(() -> new ConflitoEntidadeInexistente("Bairro não encontrado"));
+    }
+
+
 }
