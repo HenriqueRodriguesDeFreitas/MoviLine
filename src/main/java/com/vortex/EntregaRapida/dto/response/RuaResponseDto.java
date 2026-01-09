cb17a0472ad47a2f145a6b4b0e990cb4c696464c
Paulo <devhenriquerodrigues971@gmail.com>
2026-01-09 19:55:58 -0300

    feat: criando response dto para rua.

---
+++ src/main/java/com/vortex/EntregaRapida/dto/response/RuaResponseDto.java
@@ -0,0 +1,16 @@
+package com.vortex.EntregaRapida.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import java.util.UUID;
+
+public record RuaResponseDto(@Schema(description = "Nome do estado",
+        example = "Pará") String estado,
+                             @Schema(description = "Nome da cidade",
+                                     example = "Breves") String cidade,
+                             @Schema(description = "Nome da cidade",
+                                     example = "Aeroporto") String bairro,
+                             @Schema(description = "Id da rua", example = "3fa85f64-5717-4562-b3fc-2c963f66afa6") UUID id,
+                             @Schema(description = "Nome da rua",
+                                     example = "Presidente Vargas") String nome) {
+}
