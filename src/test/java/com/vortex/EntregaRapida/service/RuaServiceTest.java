47f3c074d8d45881cc223293858e8eddcb77d97d
Paulo <devhenriquerodrigues971@gmail.com>
2026-01-10 12:34:53 -0300

    feat: criando teste

---
+++ src/test/java/com/vortex/EntregaRapida/service/RuaServiceTest.java
@@ -0,0 +1,93 @@
+package com.vortex.EntregaRapida.service;
+
+import com.vortex.EntregaRapida.dto.request.RuaRequestDto;
+import com.vortex.EntregaRapida.dto.response.RuaResponseDto;
+import com.vortex.EntregaRapida.mapper.RuaMapper;
+import com.vortex.EntregaRapida.model.Bairro;
+import com.vortex.EntregaRapida.model.Cidade;
+import com.vortex.EntregaRapida.model.Estado;
+import com.vortex.EntregaRapida.model.Rua;
+import com.vortex.EntregaRapida.repository.RuaRepository;
+import com.vortex.EntregaRapida.service.validation.BairroCidadeValidator;
+import com.vortex.EntregaRapida.service.validation.CidadeEstadoValidator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class RuaServiceTest {
+
+    @Mock
+    private CidadeEstadoValidator cidadeEstadoValidator;
+    @Mock
+    private BairroCidadeValidator bairroCidadeValidator;
+    @Mock
+    private RuaRepository ruaRepository;
+    @Mock
+    private RuaMapper ruaMapper;
+
+    @InjectMocks
+    private RuaService ruaService;
+
+    private RuaRequestDto requestDto;
+    private RuaResponseDto ruaResponseDto;
+
+    private Estado estado;
+    private Cidade cidade;
+    private Bairro bairro;
+    private Rua novaRua;
+    private final UUID estadoId = UUID.randomUUID();
+    private final UUID cidadeId = UUID.randomUUID();
+    private final UUID bairroId = UUID.randomUUID();
+    private final UUID ruaId = UUID.randomUUID();
+
+    @BeforeEach
+    void setUp() {
+        requestDto =
+                new RuaRequestDto(estadoId, cidadeId, bairroId, "Tancredo Neves", "88132622");
+        estado = new Estado(requestDto.estadoId(), "Pará");
+        cidade = new Cidade(requestDto.cidadeId(), "Breves", estado);
+        bairro = new Bairro(requestDto.bairroId(), "Centro", cidade);
+        novaRua = new Rua(ruaId, requestDto.nome(), requestDto.cep(), bairro);
+    }
+
+    @Test
+    void cadastrarRua_deveRetornarRuaResponseDto_casoSucesso() {
+        when(cidadeEstadoValidator.validaCidadePertenceAoEstado
+                (requestDto.cidadeId(), requestDto.estadoId())).thenReturn(true);
+        when(bairroCidadeValidator.validaCidadePossuiBairro
+                (requestDto.cidadeId(), requestDto.bairroId())).thenReturn(true);
+        when(ruaRepository.existsByNomeIgnoreCaseAndBairroId
+                (requestDto.nome(), bairroId)).thenReturn(false);
+        when(bairroCidadeValidator.getBairroPorId(bairroId)).thenReturn(bairro);
+        when(ruaRepository.save(any(Rua.class))).thenReturn(novaRua);
+        when(ruaMapper.toResponse(any(Rua.class))).thenAnswer(invocation -> {
+            Rua rua = invocation.getArgument(0);
+            String nomeEstado = rua.getBairro().getCidade().getEstado().getNome();
+            String nomeCidade = rua.getBairro().getCidade().getNome();
+            String nomeBairro = rua.getBairro().getNome();
+            return new RuaResponseDto(nomeEstado, nomeCidade, nomeBairro, rua.getId(), rua.getNome());
+        });
+
+        ruaResponseDto = ruaService.cadastrarRua(requestDto);
+
+        assertNotNull(ruaResponseDto, "Retorno não deveria ser nulo.");
+        assertEquals(estado.getNome(), ruaResponseDto.estado(), "Nome dos estados não concidem.");
+        assertEquals(cidade.getNome(), ruaResponseDto.cidade(), "Nome das cidades não concidem.");
+        assertEquals(bairro.getNome(), ruaResponseDto.bairro(), "Nome dos bairros não concidem.");
+        assertEquals(novaRua.getId(), ruaResponseDto.id(), "Ids das ruas não concidem.");
+        assertEquals(novaRua.getNome(), ruaResponseDto.nome(), "Nomes das ruas não concidem.");
+
+        verify(ruaRepository, times(1)).save(any(Rua.class));
+    }
+}
