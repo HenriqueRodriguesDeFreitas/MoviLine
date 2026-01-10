ecbaa1d152bb6ff23bbee42a295347b8cff15119
Paulo <devhenriquerodrigues971@gmail.com>
2026-01-10 12:34:43 -0300

    fix: finalizando metodo de salvar.

--- src/main/java/com/vortex/EntregaRapida/service/RuaService.java
+++ src/main/java/com/vortex/EntregaRapida/service/RuaService.java
@@ -4,9 +4,12 @@ import com.vortex.EntregaRapida.dto.request.RuaRequestDto;
 import com.vortex.EntregaRapida.dto.response.RuaResponseDto;
 import com.vortex.EntregaRapida.exception.custom.ConflitoDeEntidadeException;
 import com.vortex.EntregaRapida.exception.custom.ConflitoEntidadeInexistente;
+import com.vortex.EntregaRapida.mapper.RuaMapper;
+import com.vortex.EntregaRapida.model.Rua;
 import com.vortex.EntregaRapida.repository.RuaRepository;
 import com.vortex.EntregaRapida.service.validation.BairroCidadeValidator;
 import com.vortex.EntregaRapida.service.validation.CidadeEstadoValidator;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -17,15 +20,19 @@ public class RuaService {
     private final CidadeEstadoValidator cidadeEstadoValidator;
     private final BairroCidadeValidator bairroCidadeValidator;
     private final RuaRepository ruaRepository;
+    private final RuaMapper ruaMapper;
 
     public RuaService(CidadeEstadoValidator cidadeEstadoValidator,
                       BairroCidadeValidator bairroCidadeValidator,
-                      RuaRepository ruaRepository) {
+                      RuaRepository ruaRepository,
+                      RuaMapper ruaMapper) {
         this.cidadeEstadoValidator = cidadeEstadoValidator;
         this.bairroCidadeValidator = bairroCidadeValidator;
         this.ruaRepository = ruaRepository;
+        this.ruaMapper = ruaMapper;
     }
 
+    @Transactional
     public RuaResponseDto cadastrarRua(RuaRequestDto request) {
         verificaCidadePertenceAoEstado(request);
         verificaCidadePossuiBairro(request);
@@ -33,11 +40,14 @@ public class RuaService {
         boolean validacaoCidadePossuiBairro
                 = verificaBairroPossuiRuaComMesmoNome(request.bairroId(), request.nome());
 
-        if(validacaoCidadePossuiBairro) {
+        if (validacaoCidadePossuiBairro) {
             throw new ConflitoDeEntidadeException("Bairro já possui rua com o mesmo nome.");
         }
 
+        var bairro = bairroCidadeValidator.getBairroPorId(request.bairroId());
 
+        Rua novaRua = new Rua(request.nome(), request.cep(), bairro);
+        return ruaMapper.toResponse(ruaRepository.save(novaRua));
     }
 
     private void verificaCidadePertenceAoEstado(RuaRequestDto request) {
