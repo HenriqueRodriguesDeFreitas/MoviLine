8729bc133b2dfdda7cf5f62819ae369dd2671ce5
Paulo <devhenriquerodrigues971@gmail.com>
2026-01-10 12:34:04 -0300

    fix: add requisição de cep.

--- src/main/java/com/vortex/EntregaRapida/dto/request/RuaRequestDto.java
+++ src/main/java/com/vortex/EntregaRapida/dto/request/RuaRequestDto.java
@@ -2,11 +2,13 @@ package com.vortex.EntregaRapida.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.util.UUID;
 
 public record RuaRequestDto(@NotNull UUID estadoId,
                             @NotNull UUID cidadeId,
                             @NotNull UUID bairroId,
-                            @NotBlank String nome) {
+                            @NotBlank String nome,
+                            @NotBlank @Size(min = 8 ,max = 8) String cep) {
 }
