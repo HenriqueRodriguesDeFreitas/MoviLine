b8d0d049c829c83a717eb4e7a55b591a8c153924
Paulo <devhenriquerodrigues971@gmail.com>
2026-01-18 01:16:14 -0300

    fix: novo contrato adicionado.

--- src/main/java/com/vortex/EntregaRapida/repository/BairroRepository.java
+++ src/main/java/com/vortex/EntregaRapida/repository/BairroRepository.java
@@ -20,6 +20,7 @@ public interface BairroRepository extends JpaRepository<Bairro, UUID> {
     boolean existsByNomeIgnoreCaseAndCidadeId(String nome, UUID cidadeId);
 
     boolean existsByIdAndCidadeId(UUID bairroId, UUID cidadeId);
+    boolean existsByCidadeId(UUID cidadeId);
 
     Optional<Bairro> findByNomeIgnoreCaseAndCidadeId(String nomeBairro, UUID cidadeId);
 
