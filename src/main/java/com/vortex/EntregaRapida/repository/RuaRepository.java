3b934ca643974b84448ee9bc22d4ce3c9fe50582
Paulo <devhenriquerodrigues971@gmail.com>
2026-01-30 16:56:43 -0300

    fix: criando busca das rua de um bairro.

--- src/main/java/com/vortex/EntregaRapida/repository/RuaRepository.java
+++ src/main/java/com/vortex/EntregaRapida/repository/RuaRepository.java
@@ -1,6 +1,8 @@
 package com.vortex.EntregaRapida.repository;
 
 import com.vortex.EntregaRapida.model.Rua;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
@@ -9,4 +11,5 @@ public interface RuaRepository extends JpaRepository<Rua, UUID> {
 
     boolean existsByIdAndBairroId(UUID ruaId, UUID bairroId);
     boolean existsByNomeIgnoreCaseAndBairroId(String nome, UUID bairroId);
+    Page<Rua> findByBairroId(UUID bairroId, Pageable pageable);
 }
